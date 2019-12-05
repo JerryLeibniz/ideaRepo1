0390b44f5b66ce82b597e530564704135a2abde4
slient <1091293809@qq.com>
2019-12-05 14:03:18 +0800

    添加第一个IDEA测试工程到本地仓库

---
+++ gitTest/src/com/itheima/FirstGit.java
@@ -0,0 +1,18 @@
+package com.itheima;
+
+/**
+ * @author WQ
+ * @create 2019-12-05 13:45
+ */
+public class FirstGit {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+    }
+}
